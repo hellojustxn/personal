355e3ce266b6c69282584753143247d2c5d04b80
Justin Lee <Lgwjustin@gmail.com>
2021-12-07 04:41:31 -0800

    Home page is done

---
+++ src/app/_____.component.jsx
@@ -0,0 +1,20 @@
+// type Props = {};
+
+// const _____ = ({}: Props) => {
+//   return (
+//     <div className="">
+//       <p></p>
+//     </div>
+//   );
+// };
+
+
+const _____ = ({}) => {
+  return (
+    <div className="">
+      <p></p>
+    </div>
+  );
+};
+
+export default _____;

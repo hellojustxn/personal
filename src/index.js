b712f732f444b312cc25aa00216f2e9759967e3c
Justin <Justin@rabbit.local>
2022-04-27 01:58:04 -0700

    anal in head

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App.component';
+// import splitbee from '@splitbee/web';
 
+// This initiliazes Splitbee.js
+// splitbee.init()
 
 ReactDOM.render(
   <React.StrictMode>
